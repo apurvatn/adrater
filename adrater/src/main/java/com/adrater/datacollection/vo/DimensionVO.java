e1551e8f5170df1fd7d7f1b5682d43964f6134b6
Apurva TN <apurvatn@gmail.com>
2014-07-22 19:42:29 -0700

    local changes

---
+++ adrater/src/main/java/com/adrater/datacollection/vo/DimensionVO.java
@@ -0,0 +1,7 @@
+package com.adrater.datacollection.vo;
+
+public class DimensionVO {
+
+	private int id;
+	private String text;
+}
---
+++ adrater/src/main/java/com/adrater/datacollection/vo/UserRatingVO.java
@@ -0,0 +1,79 @@
+package com.adrater.datacollection.vo;
+
+
+public class UserRatingVO {
+
+	private String ad_id;
+	private String user_email;
+	private int dimension1;
+	private int rating1;
+	private int dimension2;
+	private int rating2;
+	private int dimension3;
+	private int rating3;
+	private int dimension4;
+	private int rating4;
+	
+	public String getAd_id() {
+		return ad_id;
+	}
+	public void setAd_id(String ad_id) {
+		this.ad_id = ad_id;
+	}
+	public String getUser_email() {
+		return user_email;
+	}
+	public void setUser_email(String user_email) {
+		this.user_email = user_email;
+	}
+	public int getDimension1() {
+		return dimension1;
+	}
+	public void setDimension1(int dimension1) {
+		this.dimension1 = dimension1;
+	}
+	public int getRating1() {
+		return rating1;
+	}
+	public void setRating1(int rating1) {
+		this.rating1 = rating1;
+	}
+	public int getDimension2() {
+		return dimension2;
+	}
+	public void setDimension2(int dimension2) {
+		this.dimension2 = dimension2;
+	}
+	public int getRating2() {
+		return rating2;
+	}
+	public void setRating2(int rating2) {
+		this.rating2 = rating2;
+	}
+	public int getDimension3() {
+		return dimension3;
+	}
+	public void setDimension3(int dimension3) {
+		this.dimension3 = dimension3;
+	}
+	public int getRating3() {
+		return rating3;
+	}
+	public void setRating3(int rating3) {
+		this.rating3 = rating3;
+	}
+	public int getDimension4() {
+		return dimension4;
+	}
+	public void setDimension4(int dimension4) {
+		this.dimension4 = dimension4;
+	}
+	public int getRating4() {
+		return rating4;
+	}
+	public void setRating4(int rating4) {
+		this.rating4 = rating4;
+	}
+	
+	
+}
